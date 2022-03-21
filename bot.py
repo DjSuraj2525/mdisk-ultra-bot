e110d2bc208c9a78e76f76273ef361d4132809eb
DjSuraj2525 <67639957+DjSuraj2525@users.noreply.github.com>
2022-03-21 11:12:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -157,7 +157,7 @@ async def new_Doodstream_url(urls):
 async def remove_username(new_List):
     for i in new_List:
         if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i):
-            new_List.replace([i], CUSTOM_FOOTER[i])
+            new_List.replace(i, a)
     return new_List
 
 async def addFooter(str):
