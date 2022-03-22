b03b4d05513c8c847be1cf9a0be4f1d785f4bed8
DjSuraj2525 <67639957+DjSuraj2525@users.noreply.github.com>
2022-03-22 13:19:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-API_ID = environ.get('API_ID', '6')
-API_HASH = environ.get('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-CHANNEL = environ.get('CUSTOM_FOOTER')
-MDISK_TOKEN = environ.get('MDISK_TOKEN')
-bot = Client('Doodstream bot',
+MDISK_API = environ.get('MDISK_API')
+API_KEY = environ.get('API_KEY')
+CHANNEL = environ.get('CHANNEL')
+HOWTO = environ.get('HOWTO')
+bot = Client('Mdisk bot',
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
@@ -28,18 +30,30 @@ bot = Client('Doodstream bot',
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(
-        f"**Hola 👋{message.chat.first_name}!**\n\n"
-        "**A Simple PDisk Uploader Bot.\n\n➠ Send Me Any Direct Link, YouTube Link Or Video Link  I Will Upload To PDisk And Give Direct Link\n\nMade With ❤ BY @MoviesFlixers_DL**")
+        f"**Hey, {message.chat.first_name}!**\n\n"
+        "**I am a Mdisk post convertor bot and i am able to upload all direct links to Mdisk ,just send me links or full post... \n Join my Group @ComicBank**")
 
+@bot.on_message(filters.command('help') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**Hello, {message.chat.first_name}!**\n\n"
+        "**If you send post which had Mdisk Links, texts & images... Than I'll convert & replace all Mdisk links with your Mdisk/Doodstream links \nMessage me @kamdev07 For more help-**")
+
+@bot.on_message(filters.command('support') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**Hey, {message.chat.first_name}!**\n\n"
+        "**please contact me on @kamdev07 or for more join @Doodstream_Admins**")
+    
 @bot.on_message(filters.text & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.text)
-    conv = await message.reply("Processing your given URL ⚡")
+    conv = await message.reply("Converting...")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
         await bot.delete_messages(chat_id=message.chat.id, message_ids=dele)
-        await message.reply(f'**{Doodstream_link}**' , quote=True)
+        await message.reply(f'{Doodstream_link}' , quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
@@ -47,7 +61,7 @@ async def Doodstream_uploader(bot, message):
 @bot.on_message(filters.photo & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.caption)
-    conv = await message.reply("Processing your given URL ⚡")
+    conv = await message.reply("Converting...")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
@@ -56,62 +70,26 @@ async def Doodstream_uploader(bot, message):
             await message.reply(f'{Doodstream_link}' , quote=True)
         else:
             await bot.delete_messages(chat_id=message.chat.id, message_ids=dele)
-            await bot.send_photo(message.chat.id, message.photo.file_id, caption=f'**{Doodstream_link}**')
+            await bot.send_photo(message.chat.id, message.photo.file_id, caption=f'{Doodstream_link}')
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
 
-'''async def get_ptitle(url):
-    if ('bit' in url ):
-      url = urlopen(url).geturl()
-      
-      
-    html_text = requests.get(url).text
-    soup = BeautifulSoup(html_text, 'html.parser')
-    for title in soup.find_all('title'):
-        pass
-    title = list(title.get_text())
-    title = title[8:]
-    str = 't.me/' + CHANNEL + ' '
-    for i in title:
-        str = str + i
-    lst = list(html_text.split(","))
-    c = 0
-    for i in lst:
-        if ("""/e/""" in i):
-            found = lst[c]
-            break
-        c += 1
-    # Doodstream.com link
-    Doodstream_video_id = list(found.split(":"))
-    video_id = Doodstream_video_id[2]
-    video_id = list(video_id.split(","))
-    v_id = video_id[0]
-    #v_len = len(v_id)
-    #v_id = v_id[1:v_len - 2]
-    v_url = 'https://vidzoop.blogspot.com/p/share-video.html?vid=' + v_id + '&m=1'
-    v_url = url
-    res = [str, v_url]
-    return res'''
 
-
-async def Doodstream_up(link):
-    if ('bit' in link ):
-        #link = urlopen(link).geturl()
+async def Doodstream_up(links):
+    if ('bit' in links ):
+        #links = urlopen(links).geturl()
         unshortener = UnshortenIt()
-        link = unshortener.unshorten(link)
+        links = unshortener.unshorten(links)
     
-    title_new = urlparse(link)
-    title_new = os.path.basename(title_new.path)
-    title_Doodstream = '@' + CHANNEL + title_new
-    realaurl = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
-    param = {'token':f'{MDISK_TOKEN}','link':link}
-    res = requests.post(realaurl, json = param)         
-    data = res.json()
-    data = dict(data)
-    print(data)
-    #bot.delete_messages(con)
-    v_url = data['sharelink']
+    if ('entertainvideo' in links or 'mdisk' in links):
+        url = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+        param = {'token': MDISK_API,'link': links}
+        res = requests.post(url, json = param)
+        data = res.json()
+        data = dict(data)
+        v_url = data['sharelink']
+
     return (v_url)
 
 
@@ -147,21 +125,33 @@ async def multi_Doodstream_up(ml_string):
 async def new_Doodstream_url(urls):
     new_urls = []
     for i in urls:
+        #if ('entertainvideo' in urls or 'mdisk' in urls or 'bit' in urls or 'bit' in urls):
         time.sleep(0.2)
         new_urls.append(await Doodstream_up(i))
+        #else:
+            #continue
     return new_urls
 
 
 async def remove_username(new_List):
+    count = 0
     for i in new_List:
-        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i):
-            new_List.remove(i)
+        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://t.me/+' in i or 'instagram' in i or 'porn' in i):
+            count+=1
+    while(count):
+      
+        for i in new_List:
+            if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://t.me/+' in i or 'instagram' in i or 'Porn' in i):
+                new_List.remove(i)
+        count-=1
     return new_List
 
 async def addFooter(str):
     footer = """
+    ━━━━━━━━━━━━━━━
+⚙️ How to Download / Watch Online :""" + HOWTO + """
 ━━━━━━━━━━━━━━━
-✪ »JOIN CHANNEL ➡️ t.me/""" + CHANNEL
+⭐️JOIN CHANNEL ➡️ t.me/""" + CHANNEL
     return str + footer
 
 bot.run()
